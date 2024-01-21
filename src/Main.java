2259e9b67a0f83df4a76244d7e118ffdeaaea018
Stefan <stefanpefi3@gmail.com>
2024-01-21 11:31:36 +0100

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Dobrodosi u sah!");
+        Scanner sc=new Scanner(System.in);
     }
 }
