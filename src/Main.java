285dc53f9bb58d728a7758545a50b6504a00fbb3
NebojsaJeremic <neki333@gmail.com>
2024-01-21 11:21:18 +0100

    dobrodosli

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Dobrodosi u sah!");
     }
 }
